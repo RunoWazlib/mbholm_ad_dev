a85f9be9f569637fb536bba074149159a641de2b
RunoWazlib <ethanrog01@gmail.com>
2023-06-22 23:01:29 -0500

    Print array names

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -1,10 +1,21 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 from pyod.models.iforest import IForest
 import pyod.utils.data
 
 fake_data = pyod.utils.data.generate_data()
 #IForest()
-print(fake_data)
-print("[-] Done")
+#print(fake_data)
+for i in range(4):
+    if i == 0:
+        print("[*] X_train:")
+    if i == 1:
+        print("[*] X_test:")
+    if i == 2:
+        print("[*] Y_train:")
+    if i == 3:
+        print("[*] Y_test:")
+    print(f"%s \n" % fake_data[i])
+
+print("[-] Data Generated")
 
