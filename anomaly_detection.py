bca1bc5a543dbb3d58c2096b8b5485d9383e1824
RunoWazlib <ethanrog01@gmail.com>
2023-06-23 11:50:36 -0500

    Implemented IForest with test data

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -3,17 +3,31 @@ import numpy as np
 from pyod.models.iforest import IForest
 import pyod.utils.data
 
-fake_data = pyod.utils.data.generate_data()
-for i in range(4):
-    if i == 0:
-        print("[*] X_train:")
-    if i == 1:
-        print("[*] X_test:")
-    if i == 2:
-        print("[*] Y_train:")
-    if i == 3:
-        print("[*] Y_test:")
-    print(f"%s \n" % fake_data[i])
+#Generate training data
+n_train = 100
+n_features = 10
+contamination = 0.1
+X_train, _, _, _ = pyod.utils.data.generate_data(n_train, n_features, contamination=contamination)
+print("[-] Training Data Generated")
 
-print("[-] Data Generated")
+#Create instance
+model = IForest() #could put in an expected amount of anomalous data
 
+#Train model
+model.fit(X_train)
+
+#Generate test data
+n_test = 10
+n_features = 10
+X_test, _, _, _ = pyod.utils.data.generate_data(n_test, n_features, contamination=contamination)
+print("[-] Test Data Generated")
+
+#Evaluate test data for anomalies
+scores = model.decision_function(X_test)
+
+#Smaller values are less anomalous, Larger
+print("[-] Anomaly scores: ")
+i = 0
+for score in scores:
+    print(f"Score {i}: {score}")
+    i += 1
