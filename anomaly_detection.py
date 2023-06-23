6ac4c66749ca88d8cbb2e1eb3a65195b9a8334c7
RunoWazlib <ethanrog01@gmail.com>
2023-06-23 17:01:10 -0500

    Checking outputs of .generate_data()

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -1,14 +1,31 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 from pyod.models.iforest import IForest
 import pyod.utils.data
 
+#Output of .generate_data()
+print("[*] Output of .generate_data():\n")
+fake_data = pyod.utils.data.generate_data()
+print(f"X_train: \n{fake_data[0]}")
+print(f"X_test: \n{fake_data[1]}")
+print(f"y_train: \n{fake_data[2]}")
+print(f"y_test: \n{fake_data[3]}")
+
 #Generate training data
-n_train = 100
-n_features = 10
+samples = 100
+features = 10
 contamination = 0.1
-X_train, _, _, _ = pyod.utils.data.generate_data(n_train, n_features, contamination=contamination)
-print("[-] Training Data Generated")
+X_train, _, _, _ = pyod.utils.data.generate_data(n_train=samples, n_features=features, contamination=contamination)
+print("[*] Training Data Generated")
+
+#Graph X_Train data ***In Progress***
+fig, axs = plt.subplots(figsize=(5, 2.7), layout='constrained')
+y = np.array(range(0, 100))
+
+plt.show()
+axs.plot(X_train, )
+
 
 #Create instance
 model = IForest() #could put in an expected amount of anomalous data
@@ -17,10 +34,10 @@ model = IForest() #could put in an expected amount of anomalous data
 model.fit(X_train)
 
 #Generate test data
-n_test = 10
-n_features = 10
-X_test, _, _, _ = pyod.utils.data.generate_data(n_test, n_features, contamination=contamination)
-print("[-] Test Data Generated")
+samples = 10
+features = 10
+X_test, _, _, _ = pyod.utils.data.generate_data(n_test=samples, n_features=features, contamination=contamination)
+print("[*] Test Data Generated")
 
 #Evaluate test data for anomalies
 scores = model.decision_function(X_test)
